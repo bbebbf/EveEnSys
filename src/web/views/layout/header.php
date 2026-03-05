7e8d1a406486ee5a92b188984e16826e37259e22
Bernd Berends <bernd.berends@gmail.com>
2026-03-05 20:15:39 +0100

    + favicon festgelegt.

--- src/web/views/layout/header.php
+++ src/web/views/layout/header.php
@@ -10,6 +10,7 @@
   <link rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="/assets/css/app.css">
+  <link rel="icon" type="image/svg+xml" href="/assets/favicon.svg">
 </head>
 <body>
 
